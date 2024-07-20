eca820150734b31b6d85ef6f4f99bd377810cd82
franchepqm_ <74469603+franchepqm@users.noreply.github.com>
2024-07-20 15:44:49 -0300

    Create firebaseconect.js

---
+++ firebaseconect.js
@@ -0,0 +1,34 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCokvsHAozeVWEuI4we8KotMZIX86aRvjc",
+  authDomain: "alfaservicio-integral.firebaseapp.com",
+  projectId: "alfaservicio-integral",
+  storageBucket: "alfaservicio-integral.appspot.com",
+  messagingSenderId: "602530827906",
+  appId: "1:602530827906:web:0745925154ce7dd9bdee9b",
+  measurementId: "G-YJ6K2ZL3VG"
+};
+
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+
+export { firestore, auth, getDisplayName };
+
+
+signInWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log('Usuario autenticado con Google:', user);
+        window.location.href = "usuario.html";
+      })
+      .catch((error) => {
+        console.error("Error al iniciar sesión con Google: ", error.message);
+        alert("Error al iniciar sesión con Google: " + error.message);
+      });
+  }
