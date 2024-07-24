5e8bf9c600730f4d6730313996952536fc3d1e11
franchepqm_ <74469603+franchepqm@users.noreply.github.com>
2024-07-24 15:49:19 -0300

    Update firebaseconect.js

--- firebaseconect.js
+++ firebaseconect.js
@@ -1,34 +1,38 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+// firebaseconect.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app);
-const auth = getAuth(app);
+const Analytics = getAnalytics(app);
+const auth = getAuth();
 
-export { firestore, auth, getDisplayName };
+console.log("Conexión a Firebase establecida correctamente.");
 
+class ManageAccount {
+    signInWithGoogle() {
+        const provider = new GoogleAuthProvider();
+        return signInWithPopup(auth, provider)
+            .then((result) => {
+                const user = result.user;
+                console.log('Usuario autenticado con Google:', user);
+                window.location.href = "usuario.html";
+            })
+            .catch((error) => {
+                console.error("Error al iniciar sesión con Google: ", error.message);
+                alert("Error al iniciar sesión con Google: " + error.message);
+            });
+    }
+}
 
-signInWithGoogle() ;{
-    const provider = new GoogleAuthProvider();
-    return signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        console.log('Usuario autenticado con Google:', user);
-        window.location.href = "usuario.html";
-      })
-      .catch((error) => {
-        console.error("Error al iniciar sesión con Google: ", error.message);
-        alert("Error al iniciar sesión con Google: " + error.message);
-      });
-  }
+export { auth, firestore, Analytics, ManageAccount }; // Exporta ManageAccount
